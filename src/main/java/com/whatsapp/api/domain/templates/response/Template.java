478fa1a9eeed000fc038d0f5bca5aac49c323de1
Stein Desmet <stein.desmet@lin-k.net>
2024-01-03 17:32:19 +0100

    template status -> enum

--- src/main/java/com/whatsapp/api/domain/templates/response/Template.java
+++ src/main/java/com/whatsapp/api/domain/templates/response/Template.java
@@ -1,5 +1,6 @@
 package com.whatsapp.api.domain.templates.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.whatsapp.api.domain.templates.Component;
@@ -7,10 +8,13 @@ import com.whatsapp.api.domain.templates.type.Category;
 
 import java.util.List;
 
+import static com.fasterxml.jackson.annotation.JsonFormat.Feature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE;
+
 /**
  * The type Data item.
  */
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonFormat(with = READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE)
 public record Template(
 
         @JsonProperty("components") List<Component<?>> components,
@@ -23,5 +27,6 @@ public record Template(
 
         @JsonProperty("category") Category category,
         @JsonProperty("previous_category") Category previousCategory,
-        @JsonProperty("status") String status) {
+        @JsonProperty("status") TemplateStatus status
+) {
 }
